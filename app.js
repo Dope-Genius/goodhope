86beb4de16eab25c58dd77527a5104a44e1481a1
Samuel Jarvis <samueladeyemi244@gmail.com>
2021-03-06 16:12:00 -0800

    added the animation

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.menu');
+const header = document.querySelector(".header");
+
+
+btn.addEventListener('click', () => {
+  header.classList.toggle("height");
+  console.log('working');
+});
+
+AOS.init();
