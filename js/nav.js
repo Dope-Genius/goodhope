35fbdf4d2447fabde660391862f429129b0aa3e0
Samuel Jarvis <samueladeyemi244@gmail.com>
2021-04-07 00:51:40 +0100

    added seo

--- js/nav.js
+++ js/nav.js
@@ -2,19 +2,6 @@ let buttton = document.querySelectorAll('.menu');
 const body = document.querySelector(".body");
 const links = document.querySelector("#navlinks");
 
-
-
-// btn.addEventListener('click', () => {
-//   body.classList.toggle("active");
-//   links.classList.toggle("active");
-//   console.log('working');
-// });
-
-// for (var i = 0; i < btn.length; i++) {
-//   btn[i].addEventListener('click', myFunction, false);
-// };
-
-
 buttton.forEach(btn => {
   btn.addEventListener("click", myFunction)
 });
